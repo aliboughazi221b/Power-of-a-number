f944e2193527ee5394f7764c33af30932888ac50
aliboughazi221b <75165723+aliboughazi221b@users.noreply.github.com>
2020-12-08 12:41:14 +0000

    Create power-of-a-number.py

---
+++ power-of-a-number.py
@@ -0,0 +1,7 @@
+number = int(input('Enter a number: '))
+n = int(input('Enter n: '))
+p= str(n)
+y= str(number)
+def power(x):
+    print('The '+p+'th power of '+y+ ' is: '+str(x**n) )
+power(number)
